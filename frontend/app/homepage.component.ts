bb241def7096b6f949aac0b7af1cc8c8910c7dbc
user <thecodecoaster@gmail.com>
2017-05-14 16:55:43 -0400

    homepage component

---
+++ frontend/app/homepage.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'main-app',
+    template: '<h1>Freelancer Bootcamp Homepage</h1>'
+})
+
+export class HomepageComponent { }
+
