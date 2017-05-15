1de041256746da9411bd7f92101f293a4536c7b2
user <thecodecoaster@gmail.com>
2017-05-15 19:29:11 -0400

    added angular2 bootstrap and bootstrap material

--- frontend/app/app.module.ts
+++ frontend/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
 
-import { AppComponent } from './app.component'
 import { HomepageComponent } from './homepage/homepage.component';
 import { DocumentsComponent } from './documents/documents.component';
 import { ProposalListComponent } from './proposal/proposal-list.component';
@@ -15,7 +16,8 @@ import { AppRoutingModule } from './app-routing.module';
 	imports: [  //node_modules imports
 		BrowserModule,
 		AppRoutingModule,
-		FormsModule
+		FormsModule,
+		NgbModule.forRoot(),
 	],
 	declarations: [ //created components
 		AppComponent,
--- frontend/app/proposal/proposal-show.component.ts
+++ frontend/app/proposal/proposal-show.component.ts
@@ -11,12 +11,14 @@ export class ProposalShowComponent implements OnInit {
     id: number;
     routeId: any;
 
+
+    //getting the route url, so we can create a link with the id of the proposal (see .html file)    
     constructor(private route: ActivatedRoute) { }
 
     ngOnInit(): void {  //return void value -  action only
         this.routeId = this.route.params.subscribe(
             params => {
-                this.id = +params['id'];  //the + convert the value of params (url) to a number instead of string
+                this.id = +params['id'];  //the + symbol convert the value of params (url) to a number instead of string
             }
         )
     }
