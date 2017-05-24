9093695803ba38447b44240b4806bbf61dc073ca
CodeCoaster <thecodecoaster@gmail.com>
2017-05-24 19:47:07 -0400

    added document.service injectable

--- frontend/app/documents/document.service.ts
+++ frontend/app/documents/document.service.ts
@@ -2,8 +2,10 @@ import{ Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
+import { Document } from './document';
+
 @Injectable()
 
 export class DocumentService {
-
+    private documentsUrl = 'http://localhost:3003/freelance_document.json'
 }
