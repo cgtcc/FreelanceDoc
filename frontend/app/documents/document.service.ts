c31773918fc8c410105dabafd89a3df8a4ca4324
CodeCoaster <thecodecoaster@gmail.com>
2017-05-25 15:45:00 -0400

    added document.service injectable

--- frontend/app/documents/document.service.ts
+++ frontend/app/documents/document.service.ts
@@ -1,11 +1,22 @@
 import{ Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
 
-import { Document } from './document';
+import { Document } from './documents';
 
 @Injectable()
 
 export class DocumentService {
-    private documentsUrl = 'http://localhost:3003/freelance_document.json'
+    private documentsUrl = 'http://localhost:3003/freelance_document.json';
+
+    constructor(
+        private http: Http
+    ) {}
+
+    getDocuments(): Observable<Document[]>  //here we want getDocuments to return an array of Documents from the Observable
+
+        {
+            return this.http.get(this.documentsUrl)
+                            .map((response: Response) => <Document[]>response.json())  // map a response we can work with - a response of type response
+        }
 }
