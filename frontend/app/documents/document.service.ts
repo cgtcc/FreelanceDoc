7c6cda6ad7405184295af6a36bf0670405a6931a
CodeCoaster <thecodecoaster@gmail.com>
2017-05-25 15:50:08 -0400

    document.service error handling

--- frontend/app/documents/document.service.ts
+++ frontend/app/documents/document.service.ts
@@ -1,22 +1,36 @@
-import{ Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 
 import { Document } from './documents';
 
 @Injectable()
-
 export class DocumentService {
-    private documentsUrl = 'http://localhost:3003/freelance_document.json';
+	private documentsUrl = 'http://localhost:3001/freelance_documents.json';
 
-    constructor(
-        private http: Http
-    ) {}
+	constructor(
+		private http: Http
+	) {}
 
-    getDocuments(): Observable<Document[]>  //here we want getDocuments to return an array of Documents from the Observable
+	getDocuments(): Observable<Document[]> {
+		return this.http.get(this.documentsUrl)
+										.map((response: Response) => <Document[]>response.json())
+										.catch(this.handleError);
+	}
 
-        {
-            return this.http.get(this.documentsUrl)
-                            .map((response: Response) => <Document[]>response.json())  // map a response we can work with - a response of type response
-        }
+
+    // errors management (from angular documentation @ https://angular.io/docs/ts/latest/guide/server-communication.html#!#error-handling )
+	private handleError (error: Response | any) {
+    // In a real world app, we might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
 }
