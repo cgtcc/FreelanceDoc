2cb94d6897fa6061bd03503ece86c8b90d1924c7
CodeCoaster <thecodecoaster@gmail.com>
2017-06-06 15:40:16 -0400

    createProposal form submit

--- frontend/app/proposal/proposal-new.component.js
+++ frontend/app/proposal/proposal-new.component.js
@@ -5,13 +5,27 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@angular/core");
 const proposal_1 = require("./proposal");
+const proposal_service_1 = require("./proposal.service");
+const Rx_1 = require("rxjs/Rx");
 let ProposalNewComponent = class ProposalNewComponent {
-    constructor() {
+    constructor(proposalService) {
+        this.proposalService = proposalService;
         this.proposal = new proposal_1.Proposal;
         this.submitted = false;
+    }
+    createProposal(proposal) {
+        this.submitted = true;
+        this.proposalService.createProposal(proposal)
+            .subscribe(data => { return true; }, error => {
+            console.log("Error saving proposal");
+            return Rx_1.Observable.throw(error);
+        });
     }
 };
 ProposalNewComponent = __decorate([
@@ -19,8 +33,10 @@ ProposalNewComponent = __decorate([
         moduleId: module.id,
         selector: 'proposal-new',
         templateUrl: './proposal-new.component.html',
-        styleUrls: ['proposal.component.css']
-    })
+        styleUrls: ['proposal.component.css'],
+        providers: [proposal_service_1.ProposalService]
+    }),
+    __metadata("design:paramtypes", [proposal_service_1.ProposalService])
 ], ProposalNewComponent);
 exports.ProposalNewComponent = ProposalNewComponent;
 //# sourceMappingURL=proposal-new.component.js.map
--- frontend/app/proposal/proposal-new.component.ts
+++ frontend/app/proposal/proposal-new.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { Proposal } from './proposal';
+import { ProposalService } from './proposal.service';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
     moduleId: module.id,
     selector: 'proposal-new',
     templateUrl: './proposal-new.component.html',
-    styleUrls: ['proposal.component.css']
+    styleUrls: ['proposal.component.css'],
+    providers: [ ProposalService ]
 
 })
 
@@ -13,5 +16,23 @@ export class ProposalNewComponent {
     proposal = new Proposal;
     //next line concern the [hidden]="submitted" tag in the proposal-new.component.html file
     submitted: boolean = false;
+
+
+constructor (
+    private proposalService: ProposalService
+) {}
+
+createProposal(proposal:Proposal){
+    this.submitted = true;
+    this.proposalService.createProposal(proposal)
+        .subscribe(
+            data => { return true },
+            error => {
+                console.log("Error saving proposal");
+                return Observable.throw(error);
+            }
+    );
+}
+
 
 }
